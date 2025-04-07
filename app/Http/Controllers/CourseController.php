7ada24c7bf2e62f24194152f703756906c52a759
Dimuthu Sasanka Deshapriya <110438366+sasankadeshapriya@users.noreply.github.com>
2025-04-07 23:56:13 +0530

    update course controller

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -12,7 +12,8 @@ class CourseController extends Controller
      */
     public function index()
     {
-        //
+        $courses = Course::all();
+        return view('admin.courses', compact('courses'));
     }
 
     /**
---
+++ resources/views/admin/courses.blade.php
@@ -0,0 +1,15 @@
+@extends('admin.layouts.main')
+
+@section('content')
+
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          <div class="section-header">
+            <h1>Courses</h1>
+          </div>
+
+        </section>
+      </div>
+
+@endsection
